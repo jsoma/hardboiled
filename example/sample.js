15e2e1f5739bad894e66c56aa9680c0d0037f170
Jonathan Soma <jonathan.soma@gmail.com>
2013-12-28 00:22:14 -0500

    gh-pages prep

--- example/sample.js
+++ example/sample.js
@@ -1,6 +1,6 @@
 var hardboiled = require('../lib/hardboiled');
 
-hardboiled.scan("http://projects.propublica.org/docdollars/", function(err, page) {
+hardboiled.scan("http://brooklynbrainery.com/", function(err, page) {
   console.log("URL is " + page.url);
   // URL is http://handsomeatlas.com
   
